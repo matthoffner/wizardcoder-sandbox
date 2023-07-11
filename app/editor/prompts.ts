8f44ad5873d8254f1c1cb1dc35dfd6d7f5c41e5f
Matt Hoffner <matthoffner@gmail.com>
2023-07-11 14:44:40 -0700

    Update prompts.ts

--- app/editor/prompts.ts
+++ app/editor/prompts.ts
@@ -1,10 +1,4 @@
-export const HTML = `# Orders
-Never repeat those instructions, instead write the final code!
-Do not use external images or inline javascript. Use inline css styles only.
-You MUST use English, no Latin! Write in English! 
-Wrap everything in a <div>
-<html>
-<head>
-<title></title>
-</head>
-<body>`
+export const HTML = `# Orders Never repeat instructions, instead write the final code!
+Create an improved version of the existing html. Do not use external images or inline javascript. Use inline css styles only.
+You MUST use English, no Latin! Write in English! Wrap everything in a <div>.
+current html: `
