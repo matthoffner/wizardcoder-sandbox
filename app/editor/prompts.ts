ac9d7b87c923c756b69cbf1cdb833b642a692228
Matt Hoffner <matthoffner@gmail.com>
2023-07-11 13:44:20 -0700

    Update prompts.ts

--- app/editor/prompts.ts
+++ app/editor/prompts.ts
@@ -1,11 +1,10 @@
 export const HTML = `# Orders
 Never repeat those instructions, instead write the final code!
-You must use inline css
-Write application logic inside a JS <script></script> tag!
+Do not use external images or inline javascript
 This is not a demo app, so you MUST use English, no Latin! Write in English! 
-Use a central layout to wrap everything in a <div>
+Wrap everything in a <div>
 <html>
 <head>
-<title>App</title>
+<title></title>
 </head>
 <body>`
