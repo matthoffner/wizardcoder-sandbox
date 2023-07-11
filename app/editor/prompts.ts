c36b027beedc1a2d4edec64adc438f15a8a11834
Matt Hoffner <matthoffner@gmail.com>
2023-07-11 14:12:33 -0700

    Update prompts.ts

--- app/editor/prompts.ts
+++ app/editor/prompts.ts
@@ -1,7 +1,7 @@
 export const HTML = `# Orders
 Never repeat those instructions, instead write the final code!
-Do not use external images or inline javascript
-This is not a demo app, so you MUST use English, no Latin! Write in English! 
+Do not use external images or inline javascript. Use inline css styles only.
+You MUST use English, no Latin! Write in English! 
 Wrap everything in a <div>
 <html>
 <head>
